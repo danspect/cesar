e7327e4fddb95942db590ec684a1020937048c9a
Daniel Aspect <dnh77227@gmail.com>
2023-05-06 23:23:49 -0300

    refatored json

--- json_examples.py
+++ json_examples.py
@@ -4,28 +4,29 @@ import ceasar
 msg = []
 msg.append(ceasar.criptografar("Ontem teve escola", 6))
 msg.append(ceasar.criptografar("Cesar foi um dos maiores generais", 3))
-msg.append(ceasar.criptografar("Esse ano vou a escola para aprender a nadar", 15))
+msg.append(ceasar.criptografar(
+    "Esse ano vou a escola para aprender a nadar", 15))
 
 exemplos = [
     {
-        1 : [ 
-        {"mensagem" :  "Ontem teve escola"},
-        {"mensagem_criptografada" : msg[0]},
-        {"deslocamento" : 1 }
-    ]},
+        "id": 1,
+        "mensagem": "Ontem teve escola",
+        "mensagem_criptografada": msg[0],
+        "deslocamento": 1
+    },
     {
-        2 : [ 
-        {"mensagem" :  "Cesar foi um dos maiores generais"},
-        {"mensagem_criptografada" : msg[1]},
-        {"deslocamento" : 3 }
-    ]},
+        "id": 2,
+        "mensagem":  "Cesar foi um dos maiores generais",
+        "mensagem_criptografada": msg[1],
+        "deslocamento": 3
+    },
     {
-        3 : [ 
-        {"mensagem" :  "Esse ano vou a escola para aprender a nadar"},
-        {"mensagem_criptografada" : msg[2]},
-        {"deslocamento" : 15 }
-    ]}
-        ]
+        "id": 3,
+        "mensagem":  "Esse ano vou a escola para aprender a nadar",
+        "mensagem_criptografada": msg[2],
+        "deslocamento": 15
+    }
+]
 
 exemplos_json = json.dumps(exemplos)
 
